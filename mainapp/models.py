92d8380524b72550ee83bfc770efdf6e555cbc09
Yurii Semeynov <fullstack2023@yandex.ru>
2024-03-13 13:16:44 +0000

    start, test XZ

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class PlanOms(models.Model):
+    name_department = models.CharField(max_length=256, verbose_name='NameDepartmetn')
+
+    values_data = models.PositiveIntegerField(blank=True, null=True,verbose_name='ValuesData')
+
+    plan_data = models.PositiveIntegerField(blank=True, null=True,verbose_name='PlanData')
+
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name="Created")
+
+    date_update = models.DateTimeField(auto_now=True, verbose_name="Edited")
+
+    deleted = models.BooleanField(default=False)
