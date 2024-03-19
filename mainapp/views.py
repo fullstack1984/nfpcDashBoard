8ebb4ae0a5ae76a892e33cbef5d3ca298671f4df
Yurii Semeynov <fullstack2023@yandex.ru>
2024-03-19 13:46:39 +0000

    Первый рабочий столбец

--- mainapp/views.py
+++ mainapp/views.py
@@ -12,11 +12,14 @@ class MainPageView(TemplateView):
 class TablePageView(TemplateView):
     template_name = "mainapp/tables.html"
 
-    def get_context_data(self, **kwargs):        
-        context = super().get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
 
-        context["jk_data_now"] = models.PlanOms.get_values_to_db_data('jk')
-        context["kdo_data_now"] = models.PlanOms.get_values_to_db_data('kdo')
+        context = super().get_context_data(**kwargs)
+        
+        list_otd = ['kdo', 'jk', 'dsr_w', 'katamnez', 'dsr_ch', 'ot_fu_di', 'olmrd', 'vrt', 'prim_otd', 'aopb_1', 'aopb_2', 'rod_otd', 'rod_otd_1', 'afonion', 'oar', 'oritn', 'opnd', 'rkc', 'go', 'kdl']
+
+        for item in list_otd:
+            context[f"{item}_data_now"] = models.PlanOms.get_values_to_db_data(item)
         
         return context
 
@@ -24,17 +27,22 @@ class TablePageView(TemplateView):
 class PlanOmsPageView(TemplateView):
     template_name = "mainapp/planOms.html"
 
-    def post(self, request, *args, **kwargs):        
-        data_db = models.PlanOms.objects.create(
-            name_department='jk',
-            values_data=request.POST.get('jk'),
-            name_data=request.path.split('/')[2]
-        )        
-        data_db = models.PlanOms.objects.create(
-            name_department='kdo',
-            values_data=request.POST.get('kdo'),
-            name_data=request.path.split('/')[2]
-        )
+    def post(self, request, *args, **kwargs):
+        list_otd = ['kdo', 'jk', 'dsr_w', 'katamnez', 'dsr_ch', 'ot_fu_di', 'olmrd', 'vrt', 'prim_otd', 'aopb_1', 'aopb_2', 'rod_otd', 'rod_otd_1', 'afonion', 'oar', 'oritn', 'opnd', 'rkc', 'go', 'kdl']
+
+        for item in list_otd:
+            if request.POST.get(item) is not None:
+                data_db = models.PlanOms.objects.create(
+                    name_department=item,             
+                    values_data=request.POST.get(item),
+                    name_data=request.path.split('/')[2]
+                )
+            else:
+                data_db = models.PlanOms.objects.create(
+                    name_department=item,             
+                    values_data=0,
+                    name_data=request.path.split('/')[2]
+                )
         data_db.save()        
         return HttpResponseRedirect(reverse_lazy("mainapp:planOms"))
 
@@ -42,16 +50,21 @@ class PlanOmsPageView(TemplateView):
 class DataOmsPageView(TemplateView):
     template_name = "mainapp/dataOms.html"
 
-    def post(self, request, *args, **kwargs):        
-        data_db = models.PlanOms.objects.create(
-            name_department='jk',
-            values_data=request.POST.get('jk'),
-            name_data=request.path.split('/')[2]
-        )        
-        data_db = models.PlanOms.objects.create(
-            name_department='kdo',
-            values_data=request.POST.get('kdo'),
-            name_data=request.path.split('/')[2]
-        )
-        data_db.save()        
+    def post(self, request, *args, **kwargs):
+        list_otd = ['kdo', 'jk', 'dsr_w', 'katamnez', 'dsr_ch', 'ot_fu_di', 'olmrd', 'vrt', 'prim_otd', 'aopb_1', 'aopb_2', 'rod_otd', 'rod_otd_1', 'afonion', 'oar', 'oritn', 'opnd', 'rkc', 'go', 'kdl']       
+        
+        for item in list_otd:
+            if request.POST.get(item) is not None:
+                data_db = models.PlanOms.objects.create(
+                    name_department=item,             
+                    values_data=request.POST.get(item),
+                    name_data=request.path.split('/')[2]
+                )
+            else:
+                data_db = models.PlanOms.objects.create(
+                    name_department=item,             
+                    values_data=0,
+                    name_data=request.path.split('/')[2]
+                )
+        data_db.save()
         return HttpResponseRedirect(reverse_lazy("mainapp:dataOms"))
